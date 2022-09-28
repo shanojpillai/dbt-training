94fc9aef9646fe86ab29b6126f850ee2db5138b1
Shanoj <awscerti.shanoj@gmail.com>
2022-09-28 03:03:24 +0000

    report_v1

--- models/raw/raw_customer.sql
+++ models/raw/raw_customer.sql
@@ -1,8 +1,3 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
 
 select * 
 from RAW.GLOBALMART.CUSTOMER
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+select
+--from raw_orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.customerid,
+o.productid,
+o.ordersellingprice,
+o.ordercostprice,
+--from raw_customer
+c.customername,
+c.segment,
+c.country,
+--from raw_product
+p.category,
+p.productname,
+p.subcategory,
+ordersellingprice - ordercostprice as orderprofit
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
